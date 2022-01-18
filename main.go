957ade98838a0d4ece88b4fbccee017b8060dd18
mrofisr <rofim58@gmail.com>
2022-01-18 10:05:16 +0700

    update: Modify kubernetes

--- main.go
+++ main.go
@@ -16,8 +16,6 @@ func main() {
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Use(middleware.URLFormat)
 	r.Use(render.SetContentType(render.ContentTypeJSON))
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
